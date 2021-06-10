28f067bb84bdc05beda7495373bc00bbadbfea5a
Muhammad Risqullah <m.risqullah.ft19@mail.umy.ac.id>
2021-06-10 17:23:28 +0700

    act_8

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void BacaData(){
+        temanArrayList.clear();
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         JsonArrayRequest jArr = new JsonArrayRequest(url_select, new Response.Listener<JSONArray>() {
 
@@ -79,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
+                adapter.notifyDataSetChanged();
             }
         }, new Response.ErrorListener() {
             @Override
